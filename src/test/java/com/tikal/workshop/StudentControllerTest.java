2dfebab4daea15145ee4f70a6982f6c2b258ad3b
Sigal Shaharabani <sigals@tikalk.com>
2023-01-13 16:36:01 +0200

    Remove unused

--- src/test/java/com/tikal/workshop/StudentControllerTest.java
+++ src/test/java/com/tikal/workshop/StudentControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
