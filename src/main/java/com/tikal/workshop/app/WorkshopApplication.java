d72b8e63c38422d079af63779715924f677db9c6
Sigal Shaharabani <sigals@tikalk.com>
2023-01-13 16:33:28 +0200

    Solution to part 1

--- src/main/java/com/tikal/workshop/app/WorkshopApplication.java
+++ src/main/java/com/tikal/workshop/app/WorkshopApplication.java
@@ -2,12 +2,14 @@ package com.tikal.workshop.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.tikal.workshop.controller", "com.tikal.workshop.service"})
 public class WorkshopApplication {
 
     public static void main(String[] args) {
-        // TODO: start your Spring boot application
+        SpringApplication.run(WorkshopApplication.class);
     }
 
 }
