e20332db55cc6c21a012a0448c174121af5ce342
Sigal Shaharabani <sigals@tikalk.com>
2023-01-14 11:40:16 +0200

    Solution

--- src/main/java/com/tikal/workshop/app/WorkshopApplication.java
+++ src/main/java/com/tikal/workshop/app/WorkshopApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class WorkshopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WorkshopApplication.class);
+        SpringApplication.run(WorkshopApplication.class, args);
     }
 
 }
