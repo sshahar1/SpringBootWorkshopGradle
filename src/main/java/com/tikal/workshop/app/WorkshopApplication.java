bf6fb278ae6d7dc316edc24735c53b158f6ed234
Sigal Shaharabani <sigals@tikalk.com>
2023-01-20 17:16:49 +0200

    Added database

--- src/main/java/com/tikal/workshop/app/WorkshopApplication.java
+++ src/main/java/com/tikal/workshop/app/WorkshopApplication.java
@@ -2,12 +2,16 @@ package com.tikal.workshop.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @ComponentScan({"com.tikal.workshop.controller", "com.tikal.workshop.service"})
 @EnableAsync
+@EntityScan({"com.tikal.workshop.entity"})
+@EnableJpaRepositories({"com.tikal.workshop.repository"})
 public class WorkshopApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/tikal/workshop/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.tikal.workshop.repository;
+
+
+import com.tikal.workshop.entity.Student;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student, Long> {
+}
